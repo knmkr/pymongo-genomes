a58094ed46eae601682cd3c04659a9cdc22605dd
knmkr <knmkr3gma@gmail.com>
2015-01-11 18:50:22 +0900

    remove unimplemeted function

--- pymongo_genomes/lib/genome.py
+++ pymongo_genomes/lib/genome.py
@@ -30,9 +30,6 @@ class Genome(object):
         Example:
           >>> get_genotype_by_rsid([10,20,30])
           {10: 'GG', 20: '', 30: ''}
-
-        TODO:
-          - Optionally, fill reference genotype if given rsid is no in db but is in sequenced region.
         """
         if type(rsid) == int:
             rsid = [rsid]
@@ -46,45 +43,8 @@ class Genome(object):
 
         return genotypes
 
-    def get_genotype_by_chr_pos(self, chrpos):
-        pass
-
-    def is_blnak_record_sequenced(self, rs, file_format):
-        pass
-        # """Determine if genotype is `reference` or `N/A`.
-
-        # By checking if it is in sequenced region of the file_format: vcf_whole_genome, snparray_23andme, etc.
-
-        # TODO:
-        #   - Add exome region
-        # """
-
-        # return file_format == 'vcf_whole_genome'
-
-        # TODO: exome region
-        # # Extract region
-        #         region_file = os.path.join(settings.PATH_TO_INTERVAL_LIST_DIR, file_format['region_file'] + '.{0}.interval_list'.format(chrom))
-        #         extracted = extract_region(region_file, ok_records)
-        #         n_counter[file_format['short_name']] += len(extracted)
-        #         for record in extracted:
-        #             catalog.update(record, {"$set": {'is_in_' + file_format['short_name']: True}})
-
-        #         # Uniq records
-        #         ok_records_uniq = []
-        #         uniq_snps = set([rec['snp_id_current'] for rec in ok_records])
-        #         for record in ok_records:
-        #             if record['snp_id_current'] in uniq_snps:
-        #                 ok_records_uniq.append(record)
-        #                 uniq_snps.remove(record['snp_id_current'])
-        #         n_counter_uniq[file_format['short_name']] += len(extract_region(region_file, ok_records_uniq))
-
-    def get_reference_allele():
-        pass
-        # # Try to get ref(reference allele).
-        # ref = rec.get('ref')
-        # if not ref:
-        #     # log.debug('try to get allele of reference genome')
-        #     ref = self.bq.get_ref_genome(loc, rec=rec)
-        #     if not ref:
-        #         ref = na
-        #         log.warn('ref not found: loc: %s loctype: %s' % (loc, loctype))
+    # def get_genotype_by_chr_pos(self, chrpos):
+    #     """
+    #     TODO
+    #     """
+    #     pass
