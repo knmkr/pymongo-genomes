ffff0adb9373d42adeb906962b8d2afec03b54b6
knmkr <knmkr3gma@gmail.com>
2015-01-17 12:42:54 +0900

    add GenomeInfo client

---
+++ pymongo_genomes/lib/genome_info.py
@@ -0,0 +1,14 @@
+import pymongo
+
+from logger import getLogger
+log = getLogger(__name__)
+
+
+class GenomeInfo(object):
+    def __init__(self, mongo_uri=''):
+        self.con = pymongo.MongoClient(host=mongo_uri)
+        self.db = self.con.get_default_database()
+        self.genome_info = self.db['genome_info']
+
+    def get_genome_infos(self, owner):
+        return list(self.genome_info.find({'owner': owner}))
